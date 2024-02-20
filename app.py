3c53ef2e95ad0cdeeb7bc1f89f517fd1f6e1b483
DingHaotong <160452060+DingHaotong@users.noreply.github.com>
2024-02-20 14:23:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def index():
 @app.route("/main",methods=["GET","POST"])
 def main():
   name = request.from.get("name")
-  return(render_template("main.html"))
+  return(render_template("main.html",r=name))
 
 
 
